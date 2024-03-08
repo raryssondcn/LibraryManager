e162c0a24aa3f128b2829e731ff85176e0dfff04
raryssondcn <rarysson.9929@aluno.pr.senac.br>
2024-03-08 10:51:45 -0300

    adicionando BookRoutes

---
+++ src/routes/books.routes.js
@@ -0,0 +1,13 @@
+const {Router} = require("express")
+const BookController = require("../controller/BookController")
+
+const bookRoutes = Router()
+
+const bookController = new BookController
+
+bookRoutes.post("/book", bookController.createBook)
+bookRoutes.get("/books", bookController.listBooks)
+bookRoutes.put("/books/:id", bookController.updateBook)
+bookRoutes.delete("/books/:id", bookController.deleteBook)
+
+module.exports = bookController
