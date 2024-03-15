ecd1569088de862258312d7268e53ea6a6cd1fcd
raryssondcn <rarysson.9929@aluno.pr.senac.br>
2024-03-15 08:50:55 -0300

    Metodo usuario feito

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const swaggerDocument = require("../swagger.json")
 
 const app = express()
 
-app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
 app.use(routes)
 
